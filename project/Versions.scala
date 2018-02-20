319e929903612bb1ccb7747d1712bfa1a7c08ee4
Dean Wampler <dean@concurrentthought.com>
2018-02-19 19:00:09 -0600

    Bumped dependencies to the latest versions.

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,23 +5,23 @@ object Versions {
   val Scala             = "2.11.11"
   val JDK               = "1.8"
 
-  val reactiveKafkaVersion = "0.18"
-  val akkaVersion = "2.5.6"
-  val akkaHttpVersion = "10.0.10"
-  val akkaHttpJsonVersion = "1.18.1"
+  val reactiveKafkaVersion = "0.19"
+  val akkaVersion = "2.5.9"
+  val akkaHttpVersion = "10.0.11"
+  val akkaHttpJsonVersion = "1.19.0"
 
 
-  val Curator        = "4.0.0"
+  val Curator        = "4.0.1"
 
   val kafkaVersion = "1.0.0"
   val KafkaScalaVersion = "0.1.2"
 
-  val tensorflowVersion = "1.4.0"
-  val PMMLVersion = "1.3.10"
+  val tensorflowVersion = "1.5.0"
+  val PMMLVersion = "1.4.0"
 
-  val jettyVersion = "9.4.7.v20170914"
-  val jacksonVersion = "2.8.8"
-  val jerseyVersion = "2.25"
+  val jettyVersion = "9.4.8.v20171121"
+  val jacksonVersion = "2.9.4"
+  val jerseyVersion = "2.26"
   val gsonVersion = "2.8.2"
-  val wsrsVersion = "2.0.1"
+  val wsrsVersion = "2.1"
 }
